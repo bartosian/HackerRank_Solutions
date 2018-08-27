ec4349f4e3582e81d866bb50d75fcaee5c50c499
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-08-27 14:53:12 -0400

    Solution 4

--- CodeWars.js
+++ CodeWars.js
@@ -81,3 +81,29 @@ function validParentheses(parens){
 
     return stack.length === 0;
 }
+
+// TODO
+
+Array.prototype.square = function() {
+    return this.map(i => i * i);
+}
+
+Array.prototype.cube = function() {
+    return this.map(i => Math.pow(i, 3));
+}
+
+Array.prototype.average = function() {
+    return this.length === 0 ? NaN : this.reduce((sum,i) => sum+=i, 0) / this.length;
+}
+
+Array.prototype.sum = function() {
+    return this.reduce((sum,i) => sum+=i);
+}
+
+Array.prototype.even = function() {
+    return this.filter(i=>i%2===0);
+}
+
+Array.prototype.odd = function() {
+    return this.filter(i=>i%2 !== 0);
+}
