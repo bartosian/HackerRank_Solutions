c567aa70ab57e03306bf596e321b6761f2186aeb
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-08-27 14:52:12 -0400

    Solution 3

--- CodeWars.js
+++ CodeWars.js
@@ -18,3 +18,42 @@ function anagramCounter (wordsArray) {
     return counter;
 }
 
+function searchSubstr( fullText, searchText, allowOverlap ){
+    let count = 0;
+    let inProcess = true;
+    let match;
+    if(!searchText) return count;
+
+    if(!allowOverlap) {
+        while(inProcess) {
+            if(!count) {
+                match = fullText.indexOf(searchText);
+            } else {
+                match = fullText.indexOf(searchText, match + searchText.length);
+            }
+
+            if(match !== -1) {
+                count++;
+            } else {
+                inProcess = !inProcess;
+                return count;
+            }
+        }
+    }
+
+    while(inProcess) {
+        if(!count) {
+            match = fullText.indexOf(searchText);
+        } else {
+            match = fullText.indexOf(searchText, match + 1);
+        }
+
+        if(match !== -1) {
+            count++;
+        } else {
+            inProcess = !inProcess;
+            return count;
+        }
+    }
+
+}
